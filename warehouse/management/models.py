09a9937afac2668bacd3816191ba53069839d121
Alex <mwppl123@gmail.com>
2023-01-18 17:01:50 +0100

    DB plan

--- warehouse/management/models.py
+++ warehouse/management/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+#Planning warehouse db
+#Items table
+    #Item ID
+    #Item name
+    #Item size
+    #Category
+    #Item weight
+
+#Warehouses table
+    #Warehouse ID
+    #Region
+    #All Stock State
+    #All Stock Target
+
+#Stock State table
+    #Item ID
+    #Warehouse ID
+    #Stock State
+    #Stock Target
