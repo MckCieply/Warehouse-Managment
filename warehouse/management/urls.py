4f2b680da2f266ae4bd794230188e8d47ff4a946
Alex <mwppl123@gmail.com>
2023-01-20 14:23:56 +0100

    creating basic login logic

--- warehouse/management/urls.py
+++ warehouse/management/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('signup', views.signup_request, name="signup")
+    path('signup', views.signup_request, name="signup"),
+    path('login', views.login_request, name="login"),
 ]
--- warehouse/management/views.py
+++ warehouse/management/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .forms import NewUserForm
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 
 def home(response):
@@ -19,3 +20,20 @@ def signup_request(request):
         messages.error(request, "Unsuccessful registration. Invalid information.")
     form = NewUserForm()
     return render(request=request, template_name="registration/signup.html", context={"register_form":form})
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data = request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username = username, password = password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("/")
+            else:
+                messages.error(request, "Invalid username or password")
+        else:
+            messages.error(request, "Invalid username or password")
+    form = AuthenticationForm()
+    return render(request = request, template_name = "registarion/login.html", context = {"login_form":form})
