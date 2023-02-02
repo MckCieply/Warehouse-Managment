47286b31e10c8a3c0044a50f5da22f1cf00c067f
Alex <mwppl123@gmail.com>
2023-02-02 15:49:23 +0100

    Featuring logout redirect

--- warehouse/management/urls.py
+++ warehouse/management/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup', views.signup_request, name="signup"),
     path('login', views.login_request, name="login"),
-    path('logout', views.logout, name="logout"),
+    path('logout', LogoutView.as_view(template_name='registration/logout.html') , name="logout"),
 ]
--- warehouse/warehouse/settings.py
+++ warehouse/warehouse/settings.py
@@ -137,4 +137,3 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "logout"
