6b08b64f204ceea448006cae4b0a3029520ae6fc
Alex <mwppl123@gmail.com>
2023-02-14 10:40:11 +0100

    Updating models, getting ready to create DB

--- warehouse/management/models.py
+++ warehouse/management/models.py
@@ -2,19 +2,31 @@ from django.db import models
 
 # Create your models here.
 #Planning warehouse db
+class Item(models.Model):
+    item_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=70)
+    description = models.CharField(max_length=255)
+#Gotta edit category as Field.choices https://docs.djangoproject.com/en/4.1/ref/models/fields/
+    category = models.CharField(max_length=50)
 #Items table
     #Item ID
     #Item name
     #Item size
     #Category
     #Item weight
-
+class Warehouse:
+    warehouse_id = models.AutoField(primary_key=True)
+    region = models.CharField(max_length=70)
+    filled = models.DecimalField()
 #Warehouses table
     #Warehouse ID
     #Region
-    #All Stock State
-    #All Stock Target
-
+    #filled
+class Stock:
+    item_id = models.ForeignKey(Item)
+    warehouse_id = models.ForeignKey(Warehouse)
+    stock_state = models.IntegerField()
+    target_state = models.IntegerField()
 #Stock State table
     #Item ID
     #Warehouse ID
