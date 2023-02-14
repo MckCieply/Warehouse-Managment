627284b742fd4a3b472c2fead865247e7745f4a2
Alex <mwppl123@gmail.com>
2023-02-14 19:30:16 +0100

    Creating django models, migrating them to PostgreSQL

--- warehouse/management/models.py
+++ warehouse/management/models.py
@@ -3,28 +3,36 @@ from django.db import models
 # Create your models here.
 #Planning warehouse db
 class Item(models.Model):
+    category_choices = [
+        ('FOO', 'Food'),
+        ('BE', 'Beauty'),
+        ('CLO', 'Clothes'),
+        ('HE', 'Health'),
+        ('ACC', 'Accessories'),
+        ]
     item_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=70)
     description = models.CharField(max_length=255)
 #Gotta edit category as Field.choices https://docs.djangoproject.com/en/4.1/ref/models/fields/
-    category = models.CharField(max_length=50)
-#Items table
+    category = models.CharField(max_length=3,choices=category_choices)
+#Items table 
     #Item ID
     #Item name
     #Item size
     #Category
     #Item weight
-class Warehouse:
+class Warehouse(models.Model):
     warehouse_id = models.AutoField(primary_key=True)
     region = models.CharField(max_length=70)
-    filled = models.DecimalField()
+    filled = models.IntegerField()
+    max_capacity = models.IntegerField()
 #Warehouses table
     #Warehouse ID
     #Region
     #filled
-class Stock:
-    item_id = models.ForeignKey(Item)
-    warehouse_id = models.ForeignKey(Warehouse)
+class Stock(models.Model):
+    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
+    warehouse_id = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
     stock_state = models.IntegerField()
     target_state = models.IntegerField()
 #Stock State table
